21c7ebd39063cfd278e3acd74d8b6ecb188fdae7
pukarcancode <pukar.bade@nepalcancode.com>
2026-05-05 15:52:01 +0545

    WIP

--- api/services/index.ts
+++ api/services/index.ts
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-export * from "@/api/services/coachingService";
-export * from "@/api/services/questionService";
-export * from "@/api/services/todoService";
-=======
 export * from "./authService";
+export * from "./coachingService";
 export * from "./notificationPreferenceService";
 export * from "./notificationService";
 export * from "./questionService";
+export * from "./questionService";
 export * from "./todoService";
 
->>>>>>> 81115a4af58debb751faa3e85eea7e8341c47a8f
